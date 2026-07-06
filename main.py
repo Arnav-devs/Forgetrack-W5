6d8fc71dfc561a03b847f2eaa9c74d972262fe58
Arnav Kumar <Arnav.kx13@gmail.com>
2026-07-06 08:52:12 +0530

    basics

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+posts = [
+  {
+    
+  }
+]
+
+@app.route("/")
+@app.route("/home")
+def welcome():
+  return render_template('home.html')
+
+@app.route("/dashboard")
+def dashboard():
+  return render_template('dashboard.html')
+
+
+
+if __name__ == "__main__":
+ app.run(debug=True)
